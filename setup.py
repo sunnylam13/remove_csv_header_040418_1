fa91c7aff7f99a5094fdc5ba4a1822c4d8815b9b
Sunny Lam <sunny.lam@gmail.com>
2018-04-05 09:57:35 -0400

    revised setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
 	'description': 'This program removes the first line from CSV files, often the header row.  It opens every file with the `.csv` extension in the current working directory, reads in the contents and rewrites the contents without the first row...  all to a file of the same name.  This replaces the old contents of the CSV file with the new, headless content.',
 	'author': 'Sunny Lam',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
+	'url': 'https://github.com/sunnylam13/remove_csv_header_040418_1',
+	'download_url': 'https://github.com/sunnylam13/remove_csv_header_040418_1',
 	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
